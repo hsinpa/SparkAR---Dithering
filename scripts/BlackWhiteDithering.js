8188623fdfe3c1ca3b6daa5dce5c41d5bc8f96eb
hsinpa <hsinpa@gmail.com>
2020-02-04 14:02:55 +0800

    Add noise map

--- scripts/BlackWhiteDithering.js
+++ scripts/BlackWhiteDithering.js
@@ -10,10 +10,12 @@ const R = require('Reactive');
 const Materials = require('Materials');
 const Shaders = require('Shaders');
 const Patches = require('Patches');
+const Random = require('Random');
 
 //Assets
 const blurMat = Materials.get("CameraMat");
 const cameraTex = Textures.get("cameraTexture0");
+const noiseTex = Textures.get("noisemap_01");
 
 const cameraColor = cameraTex.signal;
 const texcoords = Shaders.fragmentStage(Shaders.vertexAttribute({'variableName': Shaders.VertexAttribute.TEX_COORDS}));
@@ -33,17 +35,29 @@ let LuminanceMultiplier = R.pack4(LuminanceRed,LuminanceGreen,LuminanceBlue ,0);
 
 let _ScreenParams = R.pack2(CameraInfo.previewSize.width, CameraInfo.previewSize.height);
 var sampled = Shaders.textureSampler(cameraColor, texcoords);
+var noiseSample = Shaders.textureSampler(noiseTex.signal, texcoords);
+
 let pixelCoord = R.floor(R.mul(texcoords, _ScreenParams));
 
 let grayValue = R.dot( LuminanceMultiplier, sampled);
+let noiseValue = R.mul(R.dot(noiseSample, R.pack4(1, 0, 0, 0) ), (Random.random() * 0.2));
 const kernel = [
-    [0, 0.25],
-    [0.75, 0.5]
+    [6, 8, 4],
+    [1, 0, 3],
+    [5, 2, 7],
+
 ];
+
+// const kernel = [
+//     [0, 8, 2, 10],
+//     [12, 4, 14,6],
+//     [3, 11, 1, 9],
+//     [15, 7, 13, 5]
+// ];
 
 
 let ditherColor = R.val(0);
-
+let kernelSize = 3;
 // let ditherX = isEqual( R.floor(R.mod(pixelCoord.x, 2)), 1);
 // let ditherY = isEqual( R.floor(R.mod(pixelCoord.y, 2)), 1);
 
@@ -51,19 +65,20 @@ let ditherColor = R.val(0);
 
 // Diagnostics.watch( "Hello", isDither);
 
-for (let y = 0; y < 2; y++) {
-    for (let x = 0; x < 2; x++) {
-        let ditherX = isEqual( R.round(R.mod(pixelCoord.x, 2)), x);
-        let ditherY = isEqual( R.round(R.mod(pixelCoord.y, 2)), y);
+for (let y = 0; y < kernelSize; y++) {
+    for (let x = 0; x < kernelSize; x++) {
+        let ditherX = isEqual( R.round(R.mod(pixelCoord.x, kernelSize)), x);
+        let ditherY = isEqual( R.round(R.mod(pixelCoord.y, kernelSize)), y);
         
         let isDither = isEqual(R.add(ditherX, ditherY), 2);
-        let ditherFilter = R.mul(kernel[x][y], isDither);
+        let ditherFilter = R.mul((kernel[x][y] / (kernelSize * kernelSize)), isDither);
 
         ditherColor = R.add(  R.mul(mathIfElse(ditherFilter, grayValue), isDither) , ditherColor);
         //ditherColor = R.add(isDither, ditherColor);
     }
 }
 // Diagnostics.watch( "Hello", ditherColor);
+// ditherColor = R.add(noiseValue, ditherColor);
 
 
 let output = ditherColor;
